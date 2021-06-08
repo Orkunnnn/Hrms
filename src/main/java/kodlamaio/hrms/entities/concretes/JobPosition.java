ee6fc106a4599cd5a1bde3d0cceba1ddd5e360d7
orkunnnn <orkunkonak1414@gmail.com>
2021-06-08 23:56:31 +0300

    First Commit

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/JobPosition.java
@@ -0,0 +1,18 @@
+package kodlamaio.hrms.entities.concretes;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "job_positions")
+public class JobPosition {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "position_name")
+    private String positionName;
+}
